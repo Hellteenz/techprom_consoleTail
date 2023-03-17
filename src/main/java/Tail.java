65a3e173170c4e2ad4c56cfeb592f80986be914e
Kristina <kristina.zezina.nn@gmail.com>
2023-03-18 00:01:53 +0300

    commit?

--- src/main/java/Tail.java
+++ src/main/java/Tail.java
@@ -1,30 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class Tail {
-    private Pattern patternC = Pattern.compile("\\[-c \\d\\]");
-    private Pattern patternN = Pattern.compile("\\[-n \\d\\]");
-    private Pattern patternO = Pattern.compile("\\[-o [A-z]\\]");
-    private Pattern patternFileName = Pattern.compile("[A-z]*");
 
     private List<String> files = new ArrayList<>();
     private String output;
     private Boolean commandCheck = false;
     private String command;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Tail voidTail = new Tail();
         voidTail.fileParsing(args);
-
+        if (voidTail.files.size() > 1) {
+            for (String inputFile : voidTail.files) {
+                String inputName = inputFile + ".txt";
+                FileReader input = new FileReader(inputName);
+                System.out.println(inputFile);
+                voidTail.commandChecker(voidTail, voidTail.command, input);
+            }
+        }
+        if (voidTail.files.size() == 1) {
+            String inputName = voidTail.files.get(0) + ".txt";
+            FileReader input = new FileReader(inputName);
+            voidTail.commandChecker(voidTail, voidTail.command, input);
+        }
+        else throw new FileNotFoundException("Input data hasn't any files");
     }
 
     public void fileParsing(String[] args) throws Error {
         for (String data: args) {
-            Matcher matcherC = patternC.matcher(data);
-            Matcher matcherN = patternN.matcher(data);
-            Matcher matcherFileName = patternFileName.matcher(data);
-            Matcher matcherO = patternO.matcher(data);
+            Matcher matcherC = Pattern.compile("\\[-c \\d\\]").matcher(data);
+            Matcher matcherN = Pattern.compile("\\[-n \\d\\]").matcher(data);
+            Matcher matcherFileName = Pattern.compile("[A-z]*").matcher(data);
+            Matcher matcherO = Pattern.compile("\\[-o [A-z]\\]").matcher(data);
             if (matcherC.matches() && !commandCheck) {
                 command = "c";
                 commandCheck = true;
@@ -41,5 +53,20 @@ public class Tail {
             }
             else throw new Error("Parse Error");
         }
+    }
+    public void commandChecker(Tail voidTail, String command, FileReader input) throws Error{
+        if (command == "c") {
+            voidTail.commandC(input);
+        }
+        if (command == "n") {
+            voidTail.commandN(input);
+        }
+        else throw new Error("Wrong command");
+    }
+    public void commandC(FileReader input) {
+
+    }
+    public void commandN(FileReader input) {
+
     }
 }
