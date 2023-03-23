b98f1d49c0e610c5cf16ee0f74d80e8f1bf67f1b
Kristina <kristina.zezina.nn@gmail.com>
2023-03-23 18:01:18 +0300

    fix smth in TailTest

--- src/test/java/TailTest.java
+++ src/test/java/TailTest.java
@@ -11,43 +11,35 @@ import java.nio.file.Paths;
 public class TailTest {
     String[] command;
     @Test
-    void WAP() throws IOException {
-        Tail testTail = new Tail();
-        String commandToWap = "-n 3 -o outputFile1 WAP";
-        command = commandToWap.split(" ");
-        testTail.fileParsing(testTail, command);
-        assertEquals(-1, Files.mismatch(Path.of("outputFile1"), Path.of("outputCheck/test1_WAP_3str.txt")));
-    }
-    @Test
     void Essay() throws IOException {
         Tail testTail = new Tail();
-        String commandToWap = "-c 26 -o outputFile2 Essay2";
+        String commandToWap = "-c 26 -o outputFile1 Essay2";
         command = commandToWap.split(" ");
         testTail.fileParsing(testTail, command);
-        assertEquals(-1, Files.mismatch(Path.of("outputFile2"), Path.of("outputCheck/test2_Essay2_26sym.txt")));
+        assertEquals(-1, Files.mismatch(Path.of("outputFile1"), Path.of("outputCheck/test1_Essay2_26sym.txt")));
     }
     @Test
     void Quatar() throws IOException {
         Tail testTail = new Tail();
-        String commandToWap = "-o outputFile3 Quatar";
+        String commandToWap = "-o outputFile2 Quatar";
         command = commandToWap.split(" ");
         testTail.fileParsing(testTail, command);
-        assertEquals(-1, Files.mismatch(Path.of("outputFile3"), Path.of("outputCheck/test3_Quatar_nullCommand.txt")));
+        assertEquals(-1, Files.mismatch(Path.of("outputFile2"), Path.of("outputCheck/test2_Quatar_nullCommand.txt")));
     }
     @Test
     void a_27() throws IOException {
         Tail testTail = new Tail();
-        String commandToWap = "-c 48 -o outputFile4 27a";
+        String commandToWap = "-c 48 -o outputFile3 27a";
         command = commandToWap.split(" ");
         testTail.fileParsing(testTail, command);
-        assertEquals(-1, Files.mismatch(Path.of("outputFile4"), Path.of("outputCheck/test4_27a_48sym.txt")));
+        assertEquals(-1, Files.mismatch(Path.of("outputFile3"), Path.of("outputCheck/test3_27a_48sym.txt")));
     }
     @Test
     void b_27() throws IOException {
         Tail testTail = new Tail();
-        String commandToWap = "-n 68 -o outputFile5 27b";
+        String commandToWap = "-n 68 -o outputFile4 27b";
         command = commandToWap.split(" ");
         testTail.fileParsing(testTail, command);
-        assertEquals(-1, Files.mismatch(Path.of("outputFile5"), Path.of("outputCheck/test5_27b_68str.txt")));
+        assertEquals(-1, Files.mismatch(Path.of("outputFile4"), Path.of("outputCheck/test4_27b_68str.txt")));
     }
 }
